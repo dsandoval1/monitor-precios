c102542f23c357d70e0468702df603f2579d1a86
dsandoval1 <118028748+dsandoval1@users.noreply.github.com>
2026-07-17 19:55:13 -0400

    providers.py

--- providers.py
+++ providers.py
@@ -1,1 +1,14 @@
+def buscar_producto(nombre):
+    """
+    Función temporal.
 
+    Más adelante consultará distintas tiendas y devolverá
+    el mejor precio encontrado.
+    """
+
+    return {
+        "producto": nombre,
+        "precio": 989990,
+        "tienda": "Sony Store",
+        "url": "https://store.sony.cl/"
+    }
